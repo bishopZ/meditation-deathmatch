2a69a03def755289b32f28e10c119d99e0a6a13d
bishopz <xchicago@gmail.com>
2014-05-12 23:54:48 -0500

    stop crashing on connection error

--- app.js
+++ app.js
@@ -52,20 +52,26 @@ var stopGame = function(){
 	console.log('stop')
 }
 var startConnector = function(){
-	var tgClient = nodeThinkGear.createClient({
-		appName:'NodeThinkGear',
-		appKey:'0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
-	});
-	tgClient.connect();	
-	// when think-gear sends data
-	tgClient.on('data',function(data){
-		// send it to the client browser
-		if (running) { 
-			io.emit('mindEvent', data); 
-		}
-		//console.log(data);		
-	});
-	console.log('connect')
+	try {
+
+		var tgClient = nodeThinkGear.createClient({
+			appName:'NodeThinkGear',
+			appKey:'0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
+		});
+		tgClient.connect();	
+		// when think-gear sends data
+		tgClient.on('data',function(data){
+			// send it to the client browser
+			if (running) { 
+				io.emit('mindEvent', data); 
+			}
+			//console.log(data);		
+		});
+		console.log('connect')
+	} catch(e){
+		console.log('device not connected');
+	}
+
 }
 io.sockets.on('connection', function (socket) {
 	socket.on('adapter:start', startConnector);
