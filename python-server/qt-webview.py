0d1d4ca3634275adb60d9c28d276a3b5c36f3356
Joshua Jackson <jjackson@kallisteconsulting.com>
2014-07-17 18:17:28 -0500

    Full screen, no scrollbars, start of code for menu entries

--- python-server/qt-webview.py
+++ python-server/qt-webview.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-from PyQt4.QtCore import QUrl
-from PyQt4.QtGui import QApplication
-from PyQt4.QtWebKit import QWebView
+from PyQt4 import QtGui, QtCore, QtWebKit
 
 from socketio.server import SocketIOServer
 from lib.socketiogame import SocketApp
@@ -17,16 +15,56 @@ def socketio_server(q):
     q.put(port)
     server.serve_forever()
 
+class MyWindow(QtWebKit.QWebView):
+
+  def __init__(self):
+    super(MyWindow, self).__init__()
+    self.initUI()
+ 
+  def showSelectFirstDeviceDialog(self):
+      fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file', '/dev')
+      print fname
+ 
+  def showSelectSecondDeviceDialog(self):
+      fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file', '/dev')
+      print fname
+    
+  def initUI(self):      
+    self.setWindowTitle("Meditation Deathmatch")
+
+    openFirst = QtGui.QAction(QtGui.QIcon.fromTheme("open"), 'Select First Device', self)
+    openFirst.setShortcut('Ctrl+1')
+    openFirst.setStatusTip('Select First Device')
+    openFirst.triggered.connect(self.showSelectFirstDeviceDialog)
+
+    openSecond = QtGui.QAction(QtGui.QIcon.fromTheme("open"), 'Select Second Device', self)
+    openSecond.setShortcut('Ctrl+2')
+    openSecond.setStatusTip('Select First Device')
+    openSecond.triggered.connect(self.showSelectFirstDeviceDialog)
+
+    self.menubar = QtGui.QMenuBar()
+    fileMenu = self.menubar.addMenu('&File')
+    fileMenu.addAction(openFirst)
+    fileMenu.addAction(openSecond)
+
+
+
 if __name__ == '__main__':
   q = Queue()
   ioserver = Process(target=socketio_server, args=(q,))
   ioserver.start()
   port = q.get()
   url = 'http://localhost:'+str(port)+'//client/index.html'
-  app = QApplication(sys.argv)
-  web = QWebView()
-  web.load(QUrl(url))
+
+  app = QtGui.QApplication(sys.argv)
+  web = MyWindow()
+  web.load(QtCore.QUrl(url))
+
+  web.page().mainFrame().setScrollBarPolicy(QtCore.Qt.Vertical, QtCore.Qt.ScrollBarAlwaysOff) 
+
   web.show()
-  if sys.platform == "darwin":
-    web.raise_()
-  sys.exit(app.exec_())
+  web.raise_()
+  web.showFullScreen()
+
+  app.exec_()
+  ioserver.terminate()
