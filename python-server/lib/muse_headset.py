384004a2627cd9272ebc13e7e1e113cb4d5acbd6
Joshua Jackson <jjackson@kallisteconsulting.com>
2015-04-20 18:14:45 -0500

    Switch to Muse headset

---
+++ python-server/lib/muse_headset.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+
+from gevent import monkey
+monkey.patch_all()
+import gevent
+
+import sys
+import liblo
+import datetime
+from pymindwave import pyeeg
+
+
+class MuseOSC:
+
+    def __init__(self, port=None):
+        try:
+            self.server = liblo.Server(port, liblo.TCP)
+        except liblo.ServerError as err:
+            sys.exit(str(err))
+        self.server.add_method(None, None, self.callback)
+
+    def blob_to_hex(self, b):
+        return " ".join([(hex(v/16).upper()[-1] +
+                          hex(v % 16).upper()[-1]) for v in b])
+
+    def callback(self, path, args, types, src):
+        write = sys.stdout.write
+        absolutes = ['/muse/elements/low_freqs_absolute',
+                     '/muse/elements/alpha_absolute',
+                     '/muse/elements/beta_absolute',
+                     '/muse/elements/delta_absolute',
+                     '/muse/elements/gamma_absolute',
+                     '/muse/elements/theta_absolute']
+        relatives = ['/muse/elements/alpha_relative',
+                     '/muse/elements/beta_relative',
+                     '/muse/elements/delta_relative',
+                     '/muse/elements/gamma_relative',
+                     '/muse/elements/theta_relative']
+        raw_ffts = ['/muse/elements/raw_fft0',
+                    '/muse/elements/raw_fft1',
+                    '/muse/elements/raw_fft2',
+                    '/muse/elements/raw_fft3']
+        session_scores = ['/muse/elements/alpha_session_score',
+                          '/muse/elements/beta_session_score',
+                          '/muse/elements/delta_session_score',
+                          '/muse/elements/gamma_session_score',
+                          '/muse/elements/theta_session_score']
+        if (path == '/muse/elements/experimental/concentration'):
+            self.attention = args[0]
+            #print "CONCENTRATION " + str(args[0])
+            pass
+        elif (path == '/muse/elements/experimental/mellow'):
+            self.meditation = args[0]
+            #print "MELLOW " + str(args[0])
+            pass
+        elif (path == '/muse/eeg'):
+            pass
+        elif (path == '/muse/batt'):
+            pass
+        elif (path == '/muse/eeg/dropped_samples'):
+            pass
+        elif (path == '/muse/acc'):
+            pass
+        elif (path == '/muse/config'):
+            pass
+        elif (path == '/muse/version'):
+            pass
+        elif (path == '/muse/drlref'):
+            pass
+        elif (path == '/muse/eeg/quantization'):
+            pass
+        elif (path == '/muse/elements/horseshoe'):
+            pass
+        elif (path == '/muse/elements/is_good'):
+            pass
+        elif (path == '/muse/elements/blink'):
+            pass
+        elif (path == '/muse/elements/jaw_clench'):
+            pass
+        elif (path == '/muse/elements/touching_forehead'):
+            pass
+        elif (path in absolutes):
+            pass
+        elif (path in relatives):
+            pass
+        elif (path in session_scores):
+            pass
+        elif (path in raw_ffts):
+            if (path == '/muse/elements/raw_fft0'):
+                bands = [1, 4, 8, 10, 13, 18, 31, 41, 50]
+                bin_powers, bin_relative = pyeeg.bin_power(args, bands, 220)
+                self.waves_vector = bin_powers
+                #print bin_powers
+        else:
+            write(path + " ,")
+            write(types)
+            for a, t in zip(args, types):
+                write(" ")
+                if t is None:
+                    write("[unknown type]")
+                elif t == 'b':
+                    write("[" + self.blob_to_hex(a) + "]")
+                else:
+                    write(str(a))
+            write('\n')
+
+    def fmt_timestamp(self):
+        obj = datetime.datetime
+        fmt = '%Y-%m-%d %H:%M:%S.%f'
+        return obj.now().strftime(fmt)
+
+    def run(self):
+        # just loop and dispatch messages every 10ms
+        while True:
+            self.iterate()
+            gevent.sleep(0)
+
+    def iterate(self):
+        self.server.recv(10)
+
+
+class GameHeadset(MuseOSC):
+
+    attention = 0
+    meditation = 0
+    waves_vector = [0, 0, 0, 0, 0, 0, 0, 0]
+
+    def __init__(self, *args, **kwargs):
+        MuseOSC.__init__(self, *args, **kwargs)
+
+    def disconnect(self):
+        pass
+
+    def get_json(self):
+        waves_vector = self.waves_vector
+        waves_vector = [el * 300 for el in waves_vector]
+        meditation = 100 * self.meditation
+        attention = 100 * self.attention
+        #poor_signal = self.get('poor_signal')
+        poor_signal = 0
+        packet = {
+            'eSense': {
+                'meditation': meditation,
+                'attention': attention,
+            },
+            'eegPower': {
+                'delta': waves_vector[0],
+                'theta': waves_vector[1],
+                'lowAlpha': waves_vector[2],
+                'highAlpha': waves_vector[3],
+                'lowBeta': waves_vector[4],
+                'highBeta': waves_vector[5],
+                'lowGamma': waves_vector[6],
+                'highGamma': waves_vector[7]
+            },
+            'poorSignalLevel': poor_signal
+        }
+        print packet
+        return packet
+
+
+if __name__ == '__main__':
+    #print "band,channel,value,time,whose"
+    app = MuseOSC(5001)
+    try:
+        app.run()
+    except KeyboardInterrupt:
+        del app
--- python-server/lib/socketiogame.py
+++ python-server/lib/socketiogame.py
@@ -1,51 +1,16 @@
 import os
 
-from gevent import monkey; monkey.patch_all()
+from gevent import monkey
+monkey.patch_all()
 import gevent
 
 from socketio import socketio_manage
 from socketio.namespace import BaseNamespace
 
-from pymindwave import headset
-from pymindwave.pyeeg import bin_power
+from muse_headset import GameHeadset
 
-from datetime import datetime
-import csv
+from time import sleep
 
-class GameHeadset(headset.Headset):
-  def __init__(self, *args, **kwargs):
-    headset.Headset.__init__(self, *args, **kwargs)
-
-  def raw_to_spectrum(self, rawdata):
-        flen = 50
-        spectrum, relative_spectrum = bin_power(rawdata, range(flen), 512)
-        return spectrum
-
-  def get_json(self):
-    waves_vector = self.get('waves_vector')
-    meditation = self.get('meditation')
-    attention = self.get('attention')
-    poor_signal = self.get('poor_signal')
-    spectrum = self.raw_to_spectrum(self.get('rawdata')).tolist()
-    packet = {
-      'raw_spectrum': spectrum,
-      'eSense': {
-        'meditation': meditation,
-        'attention': attention,
-      },
-      'eegPower' : {
-        'delta': waves_vector[0],
-        'theta': waves_vector[1],
-        'lowAlpha': waves_vector[2],
-        'highAlpha': waves_vector[3],
-        'lowBeta': waves_vector[4],
-        'highBeta': waves_vector[5],
-        'lowGamma': waves_vector[6],
-        'highGamma': waves_vector[7]
-      },
-      'poorSignalLevel': poor_signal
-    }
-    return packet
 
 class SocketApp(object):
     def __init__(self):
@@ -56,32 +21,33 @@ class SocketApp(object):
         path = environ['PATH_INFO'].strip('/')
 
         if not path:
-          start_response('301 Redirect', [('Location', '/client/index.html'),])
-          return []
+            headers = [('Location', '/client/index.html'), ]
+            start_response('301 Redirect', headers)
+            return []
 
         if path.startswith('client/'):
-          mode = 'rb'
-          if path.endswith(".png"):
-            content_type = "image/png"
-          elif path.endswith(".js"):
-            mode = 'r'
-            content_type = "text/javascript"
-          elif path.endswith(".css"):
-            mode = 'r'
-            content_type = "text/css"
-          elif path.endswith(".swf"):
-            content_type = "application/x-shockwave-flash"
-          else:
-            mode = 'r'
-            content_type = "text/html"
+            mode = 'rb'
+            if path.endswith(".png"):
+                content_type = "image/png"
+            elif path.endswith(".js"):
+                mode = 'r'
+                content_type = "text/javascript"
+            elif path.endswith(".css"):
+                mode = 'r'
+                content_type = "text/css"
+            elif path.endswith(".swf"):
+                content_type = "application/x-shockwave-flash"
+            else:
+                mode = 'r'
+                content_type = "text/html"
 
-          try:
-              data = open(path.replace('/', os.sep), mode).read()
-          except Exception:
-              return self.not_found(start_response)
+            try:
+                data = open(path.replace('/', os.sep), mode).read()
+            except Exception:
+                return self.not_found(start_response)
 
-          start_response('200 OK', [('Content-Type', content_type)])
-          return [data]
+            start_response('200 OK', [('Content-Type', content_type)])
+            return [data]
 
         if path.startswith("socket.io"):
             socketio_manage(environ, {'': GameNamespace}, self.request)
@@ -100,69 +66,68 @@ class GameNamespace(BaseNamespace):
     connected = False
     loop_greenlet = ''
     logfile_fd = False
-    ser1_dev = '/dev/tty.usbmodem1411'
-    ser2_dev = '/dev/tty.usbmodem1451'
-    ser1 = ''
-    ser2 = ''
     twoPlayer = False
 
     def send_ser_message(ser, message):
-      #ser.write(message + "\n")
-      print message + "\n"
+        #ser.write(message + "\n")
+        print message + "\n"
 
     def handle_lights(ser, packet):
-      arr = [
-        packet['eegPower']['delta'],
-        packet['eegPower']['theta'],
-        packet['eegPower']['lowAlpha'],
-        packet['eegPower']['highAlpha'],
-        packet['eegPower']['lowBeta'],
-        packet['eegPower']['highBeta'],
-        packet['eegPower']['lowGamma'],
-        packet['eegPower']['highGamma']
-      ]  
-      idx =  arr.index(max(arr))
-      out = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-      val = out[idx]
-      send_ser_message(ser, val)
+        arr = [
+            packet['eegPower']['delta'],
+            packet['eegPower']['theta'],
+            packet['eegPower']['lowAlpha'],
+            packet['eegPower']['highAlpha'],
+            packet['eegPower']['lowBeta'],
+            packet['eegPower']['highBeta'],
+            packet['eegPower']['lowGamma'],
+            packet['eegPower']['highGamma']
+        ]
+        idx = arr.index(max(arr))
+        out = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        val = out[idx]
+        return val  # should send to ser instead
+        #send_ser_message(ser, val)
 
     def packet_arr(self, packet):
-      return [
-        packet['eSense']['attention'],
-        packet['eSense']['meditation'],
-        packet['eegPower']['delta'],
-        packet['eegPower']['theta'],
-        packet['eegPower']['lowAlpha'],
-        packet['eegPower']['highAlpha'],
-        packet['eegPower']['lowBeta'],
-        packet['eegPower']['highBeta'],
-        packet['eegPower']['lowGamma'],
-        packet['eegPower']['highGamma']
-      ]  
+        return [
+            packet['eSense']['attention'],
+            packet['eSense']['meditation'],
+            packet['eegPower']['delta'],
+            packet['eegPower']['theta'],
+            packet['eegPower']['lowAlpha'],
+            packet['eegPower']['highAlpha'],
+            packet['eegPower']['lowBeta'],
+            packet['eegPower']['highBeta'],
+            packet['eegPower']['lowGamma'],
+            packet['eegPower']['highGamma']
+        ]
 
     def my_loop(self):
         while True:
             if (self.connected):
                 packet1 = self.hs1.get_json()
                 if (self.twoPlayer):
-                  packet2 = self.hs2.get_json()
+                    packet2 = self.hs2.get_json()
                 else:
-                  packet2 = packet1
+                    packet2 = packet1
                 packets = [packet1, packet2]
                 self.emit('mindEvent', packets)
-                self.logfile_writer.writerow(self.packet_arr(packet1) + self.packet_arr(packet2))
+                #out = self.packet_arr(packet1)
+                #out += self.packet_arr(packet2)
+                #self.logfile_writer.writerow(out)
             gevent.sleep(1)
-    
+
     def disconnect_hs(self):
         if (self.connected):
             self.connected = False
-            self.logfile_fd.close()
+            #self.logfile_fd.close()
             self.hs1.disconnect()
             if (self.twoPlayer):
-              self.hs2.disconnect()
+                self.hs2.disconnect()
             self.hs1.dongle_fs.close()
             if (self.twoPlayer):
-              self.hs2.dongle_fs.close()
+                self.hs2.dongle_fs.close()
 
     def connect_hs1(self):
         self.connect_hs()
@@ -170,45 +135,11 @@ class GameNamespace(BaseNamespace):
     def connect_hs2(self):
         self.connect_hs(True)
 
-    def connect_hs(self, twoPlayer = False):
-        self.twoPlayer = twoPlayer
-        print("twoPlayer is set to " + str(twoPlayer) + "\n") 
-        self.hs1 = GameHeadset('/dev/tty.MindWaveMobile-DevA')
-        if (self.twoPlayer):
-          self.hs2 = GameHeadset('/dev/tty.MindWaveMobile-DevA-1')
-        self.hs1.disconnect()
-        if (self.twoPlayer):
-          self.hs2.disconnect()
-        settings1 = self.hs1.dongle_fs.getSettingsDict()
-        if (self.twoPlayer):
-          settings2 = self.hs2.dongle_fs.getSettingsDict()
-        for i in xrange(2):
-            settings1['rtscts'] = not settings1['rtscts']
-            if (self.twoPlayer):
-              settings2['rtscts'] = not settings2['rtscts']
-            self.hs1.dongle_fs.applySettingsDict(settings1)
-            if (self.twoPlayer):
-              self.hs2.dongle_fs.applySettingsDict(settings2)
-        gevent.sleep(2)
-        self.hs1.connect()
-        if (self.twoPlayer):
-          self.hs2.connect()
-  
-        while self.hs1.get_state() != 'connected':
-            gevent.sleep(1)
-            print 'current state hs1: {0}'.format(self.hs1.get_state())
-        print 'hs1 connected'
-  
-        if (self.twoPlayer):
-          while self.hs2.get_state() != 'connected':
-              gevent.sleep(1)
-              print 'current state hs2: {0}'.format(self.hs2.get_state())
-          print 'hs2 connected'
-        self.logfile_fd = open('csv/md_'+datetime.now().strftime('%Y%m%d-%H%M%S')+'.csv', 'w') 
-        self.logfile_writer = csv.writer(self.logfile_fd, delimiter=',')
- 
-        self.connected = True 
-        print 'now connected!'
+    def connect_hs(self, twoPlayer=False):
+        self.hs1 = GameHeadset(5001)
+        self.hs_greenlet = gevent.Greenlet.spawn(self.hs1.run)
+        self.connected = True
+        pass
 
     def on_request_connect_one(self):
         print "will connect"
@@ -223,6 +154,6 @@ class GameNamespace(BaseNamespace):
     def on_request_disconnect(self):
         print "will disconnect"
         if ('kill' in dir(self.loop_greenlet)):
-          print "kill greenlet"
-          self.loop_greenlet.kill()
+            print "kill greenlet"
+            self.loop_greenlet.kill()
         self.disconnect_hs()
